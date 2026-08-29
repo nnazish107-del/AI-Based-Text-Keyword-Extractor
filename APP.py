44c3c32402a2e046ff44afccb8773061edf37524
nazish nazish <nnazish107@gmail.com>
2026-08-29 11:20:05 +0500

    Update APP.py

--- APP.py
+++ APP.py
@@ -37,4 +37,5 @@ def index():
                            loading=loading)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(debug=False, use_reloader=False)
